7cf4d26d342bc85d7773bf5f0ee6cf9896475e6c
CreAtIVe3334 <husu02399@gmail.com>
2022-06-08 11:32:00 +0400

    problem 2 solve

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,12 @@ import java.util.Scanner;
 public class Main {
     static Scanner scan = new Scanner(System.in);
     public static void stdin_stdout(){
-
         String st = scan.next();
         int a = scan.nextInt();;
 
         System.out.println("myString is: "+ st);
         System.out.println("myInt is: "+a);
     }
-
     public static void ifElse(){
         int k = scan.nextInt();
         if(k%2==1|| (6<k&&k<20) ){
@@ -17,10 +15,18 @@ public class Main {
         } else if((k>=2&&k<=5)||k>20){
             System.out.println("Not weird");
         }
-
+    }
+    public static void stdin_stdout2(){
+        int a= scan.nextInt();
+        float b= scan.nextFloat();
+        String c = scan.nextLine();
+        System.out.println("int: "+a);
+        System.out.println("float: "+b);
+        System.out.println("String: "+c);
     }
     public static void main(String[] args) {
         stdin_stdout();
         ifElse();
+        stdin_stdout2();
     }
 }
